20e6eecfaa28cd3499de25cf3dc085217393536a
Rafael Viegas <rafael.viegas@fatec.sp.gov.br>
2017-11-15 18:54:51 -0200

    1. Usando threads

---
+++ app/ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using ByteBank.Core.Model;
+using ByteBank.Core.Repository;
+using ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            var contas = r_Repositorio.GetContaClientes();
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+
+            var contas_1 = contas.Take(contas.Count() / 2);
+            var contas_2 = contas.Skip(contas.Count() / 2);
+
+            var thread_1 = new Thread(() =>
+            {
+                foreach (var item in contas_1)
+                    resultado.Add(r_Servico.ConsolidarMovimentacao(item));
+            });
+
+            var thread_2 = new Thread(() =>
+            {
+                foreach (var item in contas_1)
+                    resultado.Add(r_Servico.ConsolidarMovimentacao(item));
+            });
+
+            thread_1.Start();
+            thread_2.Start();
+
+            while (thread_1.IsAlive || thread_2.IsAlive)
+                Thread.Sleep(250);
+
+            var fim = DateTime.Now;
+
+            AtualizarView(resultado, fim - inicio);
+        }
+
+        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
